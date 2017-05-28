5e8a15c9c5a49181eb4c75fec360de87ca09af5e
TooLazy <napiorkowskimd@gmail.com>
2017-05-28 18:36:03 +0200

    cos tam sie wyswietla xD

--- core/src/com/nerds/nerdproject/NerdGame.java
+++ core/src/com/nerds/nerdproject/NerdGame.java
@@ -3,22 +3,26 @@ package com.nerds.nerdproject;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.uwsoft.editor.renderer.SceneLoader;
 
 
 public class NerdGame extends ApplicationAdapter {
+	private FitViewport viewport;
+	private SceneLoader sl;
 
 	@Override
 	public void create () {
-
+		viewport = new FitViewport(9, 16); // this should be the size of camera in WORLD units. make sure you check that in editor first.
+		sl = new SceneLoader(); // default scene loader loads allr esources from default RM as usual.
+		sl.loadScene("Game screen", viewport); // loading scene as usual
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        sl.getEngine().update(Gdx.graphics.getDeltaTime());
 	}
 
 	@Override
